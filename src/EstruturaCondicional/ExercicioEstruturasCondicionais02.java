6d39fef9313a11e3a79e7de13d1bdebd7374a72f
allancerqueira <allancerqueirabrito@gmail.com>
2022-07-12 16:59:52 -0300

    Exercícios estruturas condicionais 02

--- src/EstruturaCondicional/ExercicioEstruturasCondicionais02.java
+++ src/EstruturaCondicional/ExercicioEstruturasCondicionais02.java
@@ -1,5 +1,26 @@
 package EstruturaCondicional;
 
+import java.util.Scanner;
+
 public class ExercicioEstruturasCondicionais02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Digite um valor inteiro");
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int x = sc.nextInt();
+		
+		if(x % 2 == 0) {
+			System.out.println("O número digitado é par");
+		}
+		else {
+			System.out.println("O número digitado é ímpar");
+		}
+		
+		sc.close();
+		
+	}
 
 }
