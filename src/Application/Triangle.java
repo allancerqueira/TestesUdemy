d5a3ef3c0511c67ed18b11e418047874714135f4
allancerqueira <allancerqueirabrito@gmail.com>
2022-07-18 21:50:31 -0300

    criando classes

---
+++ src/Application/Triangle.java
@@ -0,0 +1,8 @@
+package Application;
+
+public class Triangle {
+	public double a;
+	public double b;
+	public double c;
+
+}
---
+++ src/EstruturasRepetitivas/Boolean.java
@@ -0,0 +1,17 @@
+package EstruturasRepetitivas;
+
+public class Boolean {
+	
+	public static void main(String[] args) {
+		
+		boolean testeLogico = 5==5;
+		System.out.println(testeLogico);
+		
+		boolean euAprendi = true;
+		
+		System.out.println(euAprendi);
+		
+		
+	}
+
+}
